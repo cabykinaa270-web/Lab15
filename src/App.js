8dbac642012da0d42fa73017c5ab1b3f71046ca7
cabykinaa270-web <cabykinaa270@gmail.com>
2026-04-16 17:13:01 +0300

    Head & Footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import Footer from './components/Footer/index.jsx';
+import Head from './components/Head/index.jsx';
+import Main from './components/Main/index.jsx';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="App"> 
+        <Head />
+        <Main />
+        <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import './style.css';
+
+export function Footer(){
+    return (
+    <div class="footer">
+        <p>16.04.2026</p>
+        <p>Chabykina_Arina_Aleksandrovna</p>
+    </div>
+    )
+}
+
+export default Footer;
